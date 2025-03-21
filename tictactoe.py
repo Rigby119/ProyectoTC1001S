ca67497b214632e8e0c7a2fc230162525f0351a4
Rigby119 <rigby119@gmail.com>
2025-03-20 21:07:57 -0600

    Agrega comentarios al código de tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -8,57 +8,69 @@ Exercises
 4. How could you create a computer player?
 """
 
+# We import the turtle library to draw on the screen
 import turtle
 
+# We import the line function from freegames
+# This function allows us to draw lines
 from freegames import line
 
 
 def grid():
     """Draw tic-tac-toe grid."""
-    line(-67, 200, -67, -200)
-    line(67, 200, 67, -200)
-    line(-200, -67, 200, -67)
-    line(-200, 67, 200, 67)
+    line(-67, 200, -67, -200)  # Vertical 1
+    line(67, 200, 67, -200)  # Vertical 2
+    line(-200, -67, 200, -67)  # Horizontal 1
+    line(-200, 67, 200, 67)  # Horizontal 2
 
 
 def drawx(x, y):
     """Draw X player."""
-    line(x, y, x + 133, y + 133)
-    line(x, y + 133, x + 133, y)
+    line(x, y, x + 133, y + 133)  # Diagonal \
+    line(x, y + 133, x + 133, y)  # Diagonal /
 
 
 def drawo(x, y):
     """Draw O player."""
-    turtle.up()
-    turtle.goto(x + 67, y + 5)
-    turtle.down()
-    turtle.circle(62)
+    turtle.up()  # Disable drawings
+    turtle.goto(x + 67, y + 5)  # Moves the turtle
+    turtle.down()  # Start drawing
+    turtle.circle(62)  # Make a circle with radius 62
 
 
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
+    # Possible return values:
+    # -200, -67, 66
 
 
-state = {'player': 0}
-players = [drawx, drawo]
+state = {'player': 0}  # Sets the first turn as x
+players = [drawx, drawo]  # Set of functions
 
 
 def tap(x, y):
     """Draw X or O in tapped square."""
-    x = floor(x)
-    y = floor(y)
-    player = state['player']
-    draw = players[player]
-    draw(x, y)
-    turtle.update()
-    state['player'] = not player
+    x = floor(x)  # Round X value
+    y = floor(y)  # Round Y value
+    player = state['player']  # Get the current player
+    draw = players[player]  # Select the shape
+    draw(x, y)  # Draws the shape in the right spot
+    turtle.update()  # Update the changes
+    state['player'] = not player  # Switch players
 
 
+# Initial setup for the screen (padding of 20px)
 turtle.setup(420, 420, 370, 0)
+# Hide the pencil from turtle
 turtle.hideturtle()
+# Disable pencil's animation
 turtle.tracer(False)
+# Draw the grid
 grid()
+# Update this change
 turtle.update()
+# Associate the tap function with clicks
 turtle.onscreenclick(tap)
+# Keep the window running
 turtle.done()
