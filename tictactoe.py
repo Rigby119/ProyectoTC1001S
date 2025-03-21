cfe626f1378bc085c65566ffca5d2450072b97b5
Rigby119 <rigby119@gmail.com>
2025-03-20 21:58:39 -0600

    Agrega la función de ganar o empatar y mostrar al ganador aparte de bloquear el juego después

--- tictactoe.py
+++ tictactoe.py
@@ -59,6 +59,48 @@ def floor(value):
     # -175, -42, 91
 
 
+def gameRule():
+    """Check if any player has won"""
+    # First check for complete columns or rows
+    for i in range(0, 3):
+        # Columns
+        if cellStates[i][0] == cellStates[i][1] == cellStates[i][2] != 0:
+            # Returns the winner value
+            return cellStates[i][0]
+        # Rows
+        if cellStates[0][i] == cellStates[1][i] == cellStates[2][i] != 0:
+            # Returns the winner value
+            return cellStates[0][i]
+    # Now it checks for diagonals
+    # Diagonal \
+    if cellStates[0][0] == cellStates[1][1] == cellStates[2][2] != 0:
+        # Returns the winner value
+        return cellStates[0][0]
+    # Diagonal /
+    if cellStates[0][2] == cellStates[1][1] == cellStates[2][0] != 0:
+        # Returns the winner value
+        return cellStates[0][2]
+    # If there's no winner still, it checks if theres still free spaces
+    if all(cell != 0 for row in cellStates for cell in row):
+        # If not, then it's a draw
+        return 3
+    # If we can still play, then it returns 0
+    return 0
+
+
+def win(value):
+    """Displays the win message and locks the screen"""
+    turtle.up()  # Stops drawing
+    turtle.goto(-200, -200)  # Moves to the lower left corner
+    if value == 1:  # If X wins
+        turtle.write("X wins!", font=("Arial", 20, "bold"))
+    elif value == 2:  # If O wins
+        turtle.write("O wins!", font=("Arial", 20, "bold"))
+    elif value == 3:  # If it's a draw
+        turtle.write("It's a Draw!", font=("Arial", 20, "bold"))
+    turtle.onscreenclick(None)  # Stops any interaction
+
+
 state = {'player': 0}  # Sets the first turn as x
 players = [drawx, drawo]  # Set of functions
 
@@ -75,7 +117,11 @@ def tap(x, y):
         draw(x, y)  # Draws the shape in the right spot
         turtle.update()  # Update the changes
         state['player'] = not player  # Switch players
-        cellStates[indX][indY] = 1
+        cellStates[indX][indY] = player + 1  # X is 1 and O is 2
+        # When the turn is over, checks if anyone has win
+        if gameRule() != 0:
+            # If so, it gives a winner
+            win(gameRule())
 
 
 # Create a Matrix for the state of each cell
